2a349b848a4f4dace08683e25e07f410ef2156da
muhammadjufry <sayyidmuhammad878@gmail.com>
2024-03-12 12:35:28 +0700

    fix some issues

--- packages/frontend/components/Header.tsx
+++ packages/frontend/components/Header.tsx
@@ -10,7 +10,18 @@ import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import { Session } from "next-auth";
-import { signOut } from "next-auth/react";
+// import { signOut } from "next-auth/react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
 
 type Props = {
   session: Session | null;
@@ -45,7 +56,7 @@ function Header({ session }: Props) {
           onMouseLeave={() => setHovering(null)}
           className={`${
             isMenuOpen ? "visible opacity-100" : "invisible opacity-0"
-          } flex lg:items-center lg:gap-4 lg:relative absolute lg:top-0 top-[83px] left-0 lg:flex-row flex-col w-full lg:w-fit lg:p-0 p-2 gap-4 lg:drop-shadow-none lg:opacity-100 lg:visible drop-shadow-xl transition-all duration-300`}
+          } flex lg:items-center lg:gap-4 lg:relative absolute lg:top-0 top-[83px] left-0 lg:flex-row flex-col w-full lg:w-fit lg:p-0 p-2 gap-4 lg:drop-shadow-none lg:opacity-100 lg:visible drop-shadow-xl transition-all duration-300 bg-white`}
         >
           <li
             className="cursor-pointer max-lg:text-lg text-md dark:hover:bg-gray-800 hover:bg-slate-100 p-2 px-3 rounded-md transition-all duration-300"
@@ -75,7 +86,7 @@ function Header({ session }: Props) {
               Pricing
             </li>
           </Link>
-          {session !== null ? (
+          {/* {session !== null ? (
             <>
               <li
                 className="max-lg:text-lg text-md dark:hover:bg-gray-800 hover:bg-slate-100 p-2 px-3 rounded-md transition-all duration-300 cursor-pointer"
@@ -93,22 +104,36 @@ function Header({ session }: Props) {
               </Link>
             </>
           ) : (
-            <>
-              <Link href="/auth/login" className="lg:hidden">
-                <li className="max-lg:text-lg text-md dark:hover:bg-gray-800 hover:bg-slate-100 p-2 px-3 rounded-md transition-all duration-300">
-                  Login
-                </li>
-              </Link>
-              <Link href="/auth/register" className="lg:hidden">
-                <li className="max-lg:text-lg text-md p-2 px-3">
-                  <button className="py-3 px-5 bg-gradient-to-r from-green-500 to-sky-500 text-white rounded-full hover:shadow-lg transition-all duration-300 flex items-center gap-1">
-                    Get Started
-                    <IconArrowRight size={18} />
-                  </button>
-                </li>
-              </Link>
-            </>
-          )}
+          <>*/}
+
+          {/* <li className="max-lg:text-lg text-md dark:hover:bg-gray-800 hover:bg-slate-100 p-2 px-3 rounded-md transition-all duration-300">
+            Login
+          </li> */}
+          <AlertDialog>
+            <AlertDialogTrigger className="lg:hidden">
+              <li className="max-lg:text-lg text-md p-2 px-3">
+                <button className="py-3 px-5 bg-gradient-to-r from-green-500 to-sky-500 text-white rounded-full hover:shadow-lg transition-all duration-300 flex items-center gap-1">
+                  Get Started
+                  <IconArrowRight size={18} />
+                </button>
+              </li>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Sorry</AlertDialogTitle>
+                <AlertDialogDescription>
+                  We{"'"}re currenly facing some issues in our authentication
+                  pages
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+
+          {/* </>
+         )}  */}
 
           <div
             style={{ left: popoverLeft ?? 0, height: popoverHeight ?? 0 }}
@@ -149,7 +174,7 @@ function Header({ session }: Props) {
           </div>
         </ul>
         <div className="items-center gap-8 lg:flex hidden">
-          {session !== null ? (
+          {/* {session !== null ? (
             <>
               <Link href="/dashboard">
                 <button className="py-3 px-5 bg-gradient-to-r from-green-500 to-sky-500 text-white rounded-full hover:shadow-lg transition-all duration-300 flex items-center gap-1">
@@ -162,18 +187,34 @@ function Header({ session }: Props) {
               </button>
             </>
           ) : (
-            <>
-              <Link href="/auth/login">
-                <button>Login</button>
-              </Link>
-              <Link href="/auth/register">
-                <button className="py-3 px-5 bg-gradient-to-r from-green-500 to-sky-500 text-white rounded-full hover:shadow-lg transition-all duration-300 flex items-center gap-1">
-                  Get Started
-                  <IconArrowRight size={18} />
-                </button>
-              </Link>
-            </>
-          )}
+            <> */}
+          {/* <Link href="/auth/login">
+            <button>Login</button>
+          </Link> */}
+          {/* <Link href="/auth/register"> */}
+          <AlertDialog>
+            <AlertDialogTrigger>
+              <button className="py-3 px-5 bg-gradient-to-r from-green-500 to-sky-500 text-white rounded-full hover:shadow-lg transition-all duration-300 flex items-center gap-1">
+                Get Started
+                <IconArrowRight size={18} />
+              </button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Sorry</AlertDialogTitle>
+                <AlertDialogDescription>
+                  We{"'"}re currenly facing some issues in our authentication
+                  pages
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          {/* </Link> */}
+          {/* </>
+          )} */}
         </div>
         <button
           className="bg-gradient-to-r from-green-500 to-sky-500 text-white p-3 rounded-full cursor-pointer lg:hidden"
